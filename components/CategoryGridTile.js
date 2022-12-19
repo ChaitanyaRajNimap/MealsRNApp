ddefc6097d67712b3c5f38dc4ca3a755acbccd9f
ChaitanyaRajNimap <chaitanyarajiwade@nimapinfotech.com>
2022-12-19 18:49:05 +0530

    Navigation and default screen added

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,51 @@
+import { View, Pressable, Text, StyleSheet, Platform } from "react-native";
+
+function CategoryGridTile({ title, color, onPress }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        onPress={onPress}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    height: 150,
+    borderRadius: 8,
+    margin: 16,
+    flex: 1,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  button: { flex: 1 },
+  buttonPressed: { opacity: 0.5 },
+  innerContainer: {
+    padding: 16,
+    borderRadius: 8,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,33 @@
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+import { CATEGORIES } from "../data/dummy-data";
+
+//Screen for displaying categories
+
+function CategoriesScreen({ navigation }) {
+  //helper fun for rendering flat list items
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview"); //To navigate to next screen
+    }
+
+    return (
+      <CategoryGridTile
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
